1998fe7f705c18ba576082d6db36f8723e40c664
joelrodriguezguzman <joel_rodriguez@outlook.com>
2025-08-03 22:46:14 -0400

    update marketplace unique name

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 
 suite('Markdown Extension Test Suite', () => {
   test('Extension should activate and register markdown.openFile command', async () => {
-	const ext = vscode.extensions.getExtension('your-publisher.markdown');
+	const ext = vscode.extensions.getExtension('joelrodriguezguzman.markdown-view');
 	assert.ok(ext, 'Extension not found');
 	await ext!.activate();
 	const commands = await vscode.commands.getCommands(true);
